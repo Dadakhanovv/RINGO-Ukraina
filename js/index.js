9840b757dd2fbe50d28ab903d14065fda10bba93
Dadakhanovv <saidumar.dadakhanov-08@outlook.com>
2024-11-26 00:51:40 +0500

    add new project

---
+++ js/index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+        width: {
+          DEFOULT: "100%",
+          tablet: "600px",
+          largetablet: "960px",
+          desktop: "1200px",
+        },
+        padding: {
+          DEFOULT: "20px",
+        },
+      },
+      screens: {
+        tablet: "640px",
+        largetablet: "1000px",
+        desktop: "1240px",
+      },
+      extends: {
+        colors: {},
+      },
+    },
+};
